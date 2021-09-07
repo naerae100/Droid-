b0eaaf030b38a67b9b51078e85bf822f9033360e
Nemi Nem <naerae100@gmail.com>
2021-09-07 22:01:49 +0545

    Login and Sign up was Created

---
+++ TheNCollection/userlogin/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.homepage),
+    path('register', views.register_user),
+    path('login', views.login_user),
+    path('logout', views.logout_user),
+    path('admins/users',views.get_users),
+    path('admins/admins', views.get_admins),
+    path('promote_user/<int:user_id>', views.promote_user),
+    path('demote_user/<int:user_id>', views.demote_user),
+    path('profile', views.profile),
+    path('password_change', auth_views.PasswordChangeView.as_view(
+        template_name='userlogin/password_change.html')),
+    path('password_change_done', auth_views.PasswordChangeView.as_view(
+        template_name='userlogin/password_change_done.html'), name='password_change_done'),
+
+]
